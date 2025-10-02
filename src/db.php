cf9bd13f9b125fe18e04ec94e32ddfc177a23c2a
Higor Julio <higorjulio34@gmail.com>
2025-10-02 18:10:33 -0300

    add authcontroller

--- src/db.php
+++ src/db.php
@@ -14,6 +14,29 @@ $options = [
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Cria a tabela users caso não exista
+    $pdo->exec("CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        type ENUM('comprador','vendedor') NOT NULL
+    )");
+    $pdo-> exec("CREATE TABLE IF NOT EXISTS rooms (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL,
+        title VARCHAR(100) NOT NULL,
+        location VARCHAR(100) NOT NULL,
+        size INT NOT NULL,
+        price DECIMAL(10,2) NOT NULL,
+        description TEXT,
+        image VARCHAR(255),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+    
+    ");
 } catch (\PDOException $e) {
     //cria um banco de dados se ele não existir
     if ($e->getCode() == 1049) { 
