568be85d55f88784a4abd3a235f336b154a91a35
Higor Julio <higorjulio34@gmail.com>
2025-10-03 20:43:49 -0300

    feat/admin

--- quartos.php
+++ quartos.php
@@ -1,5 +1,4 @@
 <?php
 require_once "src/start.php";
-require_once "src/db.php";
 require_once "templates/header.html";
 ?>
--- src/db.php
+++ src/db.php
@@ -22,7 +22,7 @@ try {
         name VARCHAR(100) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
-        type ENUM('comprador','vendedor') NOT NULL
+        type ENUM('comprador','vendedor', 'admin') NOT NULL
     )");
     $pdo-> exec("CREATE TABLE IF NOT EXISTS rooms (
         id INT AUTO_INCREMENT PRIMARY KEY,
