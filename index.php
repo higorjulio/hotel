7031186a0d4e25cb276190714b90034ebf0eb88e
Anderson <joseanderson09999@gmail.com>
2025-11-10 16:39:44 -0300

    style da pagina inicial e de login feita, com ajustes no index.php, header.html, login.php e pagina de login.css adcionada

--- index.php
+++ index.php
@@ -4,3 +4,13 @@ require_once __DIR__ . '/templates/header.html';
 ?>
 
 <h1>Bem-vindo ao Hotelys!</h1>
+<p>Hotel BelMar tem o prazer em ser o único hotel hospedagem em jão Pessoa - PB. Possuindo DNA da região em nosso nome, nosso complexo de hospedagens junta a praticidade de estar no centro da cidade com a tranquilidade da natureza presente por todo o hotel.
+Seja para uma aventura em família, uma escapada romântica ou tempo de lazer em contato com a natureza, oferecemos estrutura completa para proporcionar bem-estar, diversão e satisfação do começo ao fim de seu período conosco!
+Faça a escolha certa e hospede-se no Hotel BelMar para conhecer tudo que Jão Pessoa tem a lhe oferecer!</p>
+ 
+
+<footer class="footer text-center py-4">
+    <div class="container">
+      <p class="mb-0">© 2025 Hotelys – Todos os direitos reservados</p>
+    </div>
+  </footer>
